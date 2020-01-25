4e4791bcb78d03d4106cb7837e1076b98f701d0f
cclimcile <47402981+cclimcile@users.noreply.github.com>
2020-01-24 16:24:27 -0800

    Jan 24, 2020 4:30 PM

--- src/Practice01MathIterative.java
+++ src/Practice01MathIterative.java
@@ -1,44 +1,46 @@
 public class Practice01MathIterative implements Practice01Math {
 
     public int fib (int n) throws Exception{
+        // fib_num is the num at index n
+        int fib_num = 0;
+        // prev_num starts at 1 with the assumption that n > 1
+        int prev_num = 1;
+        // prevprev_num starts at 0 with the assumption that n > 1
+        int prevprev_num = 0;
 
-        int fib_num = 1;
-        int num1, num2 = 0;
-
+        // StackOverflowError if n < 0
         if(n < 0){
             throw new Exception();
         }
-
         if(n == 0){
             return 0;
         }
-
         if(n == 1){
             return 1;
         }
 
-        for(int i = 1; i < n; i++){
-            num1 = num2;
-            num2 = fib_num;
-            fib_num = num1 + num2;
+        for(int i = 2; i <= n; i++){
+            // fib_num stores current num for index n
+            fib_num = prev_num + prevprev_num;
+            // prevprev_num is shifted to the next num
+            prevprev_num = prev_num;
+            // prev_num is shifted to the next num
+            prev_num = fib_num;
         }
 
         return fib_num;
     }
 
     public int fact (int n) throws Exception{
-
+        // fact_sum is 1 with the assumption that n > 0
         int fact_sum = 1;
 
+        // StackOverflowError if n < 0
         if(n < 0){
             throw new Exception();
         }
 
-        if(n == 1){
-            return 1;
-        }
-
-        for(int i = n; i > 1; i--){
+        for(int i = n; i >= 1; i--){
             fact_sum *= i;
         }
 
--- src/Practice01MathRecursive.java
+++ src/Practice01MathRecursive.java
@@ -1,32 +1,34 @@
 public class Practice01MathRecursive implements Practice01Math {
 
     public int fib (int n) throws Exception{
-
+        // StackOverflowError if n < 0
         if(n < 0){
             throw new Exception();
         }
 
+        // Base case 1
         if(n == 0) {
             return 0;
         }
-
+        // Base case 2
         if(n == 1){
             return 1;
         }
-
+        // Recursive case
         return fib(n-1) + fib(n-2);
     }
 
     public int fact (int n) throws Exception{
-
+        // StackOverflowError if n < 0
         if(n < 0){
             throw new Exception();
         }
-
+        
+        // Base case
         if(n == 1){
             return 1;
         }
-
+        // Recursive case
         return n * fact(n-1);
     }
 }
